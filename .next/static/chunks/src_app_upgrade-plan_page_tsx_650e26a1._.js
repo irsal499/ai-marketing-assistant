04f0656475b5de22fef73618f5d783a1013cef73
irsal499 <2020997@iub.edu.bd>
2025-06-06 14:20:09 +0600

    Add .next/static/chunks/src_app_upgrade-plan_page_tsx_650e26a1._.js 1749198002556

---
+++ .next/static/chunks/src_app_upgrade-plan_page_tsx_650e26a1._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_d59ba681._.js",
+  "static/chunks/src_a1cb4ee6._.js"
+],
+    source: "dynamic"
+});
