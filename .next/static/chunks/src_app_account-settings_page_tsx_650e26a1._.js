2d6eb0bb4f72571cce41173073696bb148807757
irsal499 <2020997@iub.edu.bd>
2025-06-06 14:20:39 +0600

    Add .next/static/chunks/src_app_account-settings_page_tsx_650e26a1._.js 1749198032757

---
+++ .next/static/chunks/src_app_account-settings_page_tsx_650e26a1._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_@supabase_node-fetch_browser_bd451300.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_@supabase_auth-js_dist_module_33324aae._.js",
+  "static/chunks/node_modules_c030a95f._.js",
+  "static/chunks/src_71b9ceca._.js"
+],
+    source: "dynamic"
+});
